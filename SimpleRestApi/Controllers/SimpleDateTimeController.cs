54c62c04afb9586558054af88ad18682e2a83cca
Gedion <gedion.demmissie@pacshealth.com>
2019-05-28 01:59:05 -0700

    Forcing json format for the responses returned by SimpleDateTimeController's actions

--- SimpleRestApi/Controllers/SimpleDateTimeController.cs
+++ SimpleRestApi/Controllers/SimpleDateTimeController.cs
@@ -9,6 +9,7 @@ namespace SimpleRestApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
     public class SimpleDateTimeController : ControllerBase
     {
         // GET api/simpledatetime
